39fb1dfd82744b051e0a6214e6ffd36aad10c780
Kabakov Ivan <hexwhyzet@gmail.com>
2019-02-04 23:15:19 +0300

    При парсинге сайта появлялся лишний символ

--- schedule_parser.py
+++ schedule_parser.py
@@ -21,7 +21,7 @@ def get_current_schedule():
             lessons_this_day = schedule_html.find_all(class_='schedule__day__content__lesson__data')
             lessons = []
             for k in range(len(lessons_this_day)):
-                lessons.append(lessons_this_day[k].find_all('span')[0].contents[0])
+                lessons.append(lessons_this_day[k].find_all('span')[0].contents[0].strip())
             schedule_for_class.append({day: lessons})
             schedule_html.decompose()
         schedule.append({i: schedule_for_class})
