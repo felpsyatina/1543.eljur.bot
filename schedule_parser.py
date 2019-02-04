2af41ce01d3a3d551f1473b6ae62a30bd7d7e18c
Kabakov Ivan <hexwhyzet@gmail.com>
2019-02-04 23:11:56 +0300

    Запихнул все в функцию чтобы с этим можо было работать вне файла

--- schedule_parser.py
+++ schedule_parser.py
@@ -6,24 +6,22 @@ classes = ['5А', '5Б', '5В', '5Г', '6А', '6Б', '6В', '6Г', '7А', '7Б',
 schedule = []
 
 
-for i in classes:
-    print(i)
-    page = requests.get('https://1543.eljur.ru/class.' + i + '/startdate.2019-01-14/journal-schedule-action')
-    soup = BeautifulSoup(page.text, 'html.parser')
+def get_current_schedule():
+    for i in classes:
+        print(i)
+        page = requests.get('https://1543.eljur.ru/class.' + i + '/startdate.2019-01-14/journal-schedule-action')
+        soup = BeautifulSoup(page.text, 'html.parser')
 
-    schedule_for_class = []
+        schedule_for_class = []
 
-    for j in range(6):
-        schedule_html = soup.find(class_='schedule__day__content')
-        day_list = schedule_html.find_all('p')[1]
-        day = day_list.contents[0]
-        lessons_this_day = schedule_html.find_all(class_='schedule__day__content__lesson__data')
-        lessons = []
-        for k in range(len(lessons_this_day)):
-            lessons.append(lessons_this_day[k].find_all('span')[0].contents[0])
-        schedule_for_class.append({day: lessons})
-        schedule_html.decompose()
-    schedule.append({i: schedule_for_class})
-
-print(schedule)
-
+        for j in range(6):
+            schedule_html = soup.find(class_='schedule__day__content')
+            day_list = schedule_html.find_all('p')[1]
+            day = day_list.contents[0]
+            lessons_this_day = schedule_html.find_all(class_='schedule__day__content__lesson__data')
+            lessons = []
+            for k in range(len(lessons_this_day)):
+                lessons.append(lessons_this_day[k].find_all('span')[0].contents[0])
+            schedule_for_class.append({day: lessons})
+            schedule_html.decompose()
+        schedule.append({i: schedule_for_class})
