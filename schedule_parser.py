97bd79852a81f2fd19ed63ceccce14496d398139
tirlimster <tirlimster@gmail.com>
2019-02-05 14:42:13 +0300

    Сделал словарь словарей словарей (если вдруг парсилка еще будет нужна)

--- schedule_parser.py
+++ schedule_parser.py
@@ -3,28 +3,39 @@ from bs4 import BeautifulSoup
 
 classes = ['5А', '5Б', '5В', '5Г', '6А', '6Б', '6В', '6Г', '7А', '7Б', '7В', '7Г', '8А', '8Б', '8В', '8Г', '9А', '9Б',
            '9В', '9Г', '10А', '10Б', '10В', '10Г', '11А', '11Б', '11В', '11Г']
-
+DAYS = 6
+CNT_LESSONS = 7
 
 def get_current_schedule():
-    schedule = []
+    schedule = {}
 
     for i in classes:
-        page = requests.get('https://1543.eljur.ru/class.' + i + '/startdate.2019-01-14/journal-schedule-action')
+        print(i)
+        page = requests.get('https://1543.eljur.ru/class.' + i + '/startdate.2019-01-21/journal-schedule-action')
         soup = BeautifulSoup(page.text, 'html.parser')
 
-        schedule_for_class = []
+        schedule_for_class = {}
 
-        for j in range(6):
+        for j in range(DAYS):
             schedule_html = soup.find(class_='schedule__day__content')
             day_list = schedule_html.find_all('p')[1]
             day = day_list.contents[0]
             lessons_this_day = schedule_html.find_all(class_='schedule__day__content__lesson__data')
-            lessons = []
+            lessons = {}
             for k in range(len(lessons_this_day)):
-                lessons.append(lessons_this_day[k].find_all('span')[0].contents[0].strip())
-            schedule_for_class.append({day: lessons})
+                lessons[k] = lessons_this_day[k].find_all('span')[0].contents[0].strip()
+            schedule_for_class[day] = lessons
             schedule_html.decompose()
-        schedule.append({i: schedule_for_class})
+        schedule[i] = schedule_for_class
     return schedule
 
+if __name__ == '__main__':
+    schedule = get_current_schedule()
+
+    # Пример:
+    while 1:
+        cl = input()
+        day = input()
+        for k in range(CNT_LESSONS):
+            print(schedule[cl][day][k])
 
