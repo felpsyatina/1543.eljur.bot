0c9014b32062962e9c74eece4f540a2f008ffc6e
Victor Matyukhin <vam1543@yandex-team.ru>
2019-02-15 09:27:16 +0300

    Added telegram parse mode for markdown in telegram_alert function

--- logger.py
+++ logger.py
@@ -3,14 +3,17 @@ from datetime import datetime
 import config
 import requests
 
-def telegram_alert(module, log_line):
+def telegram_alert(module, log_line, tg_parse_mode = None):
 	# Special bot will sent special alert to specified technical chat or resposible person
 	# in telegram about "all crashed!"  
-	req = "https://api.telegram.org/bot" + config.secret["tg_technical"]["token"] + "/sendMessage?chat_id="
+	# tg_parse_mode can be "HTML" or "Markdown"
+	req = "https://api.telegram.org/bot" + config.secret["tg_technical"]["token"] + "/sendMessage?"
+	if tg_parse_mode:
+		req = req + "parse_mode=" + tg_parse_mode + "&"
 	chat_id = config.secret["tg_technical"]["chats"].get(module, None)
 	if not chat_id:
 		chat_id = config.secret["tg_technical"]["default_chat"]
-	req = req + str(chat_id) + "&text=" + log_line
+	req = req + "chat_id=" + str(chat_id) + "&text=" + log_line
 	sys.stderr.write("Request: " + req +"\n")
 	res = requests.get(req, verify=False)
 	sys.stderr.write("Result code " + str(res.status_code) + "\n")
