c1e30d5ba62ee6100fbf8d3a60a19315b580f534
PetkaKotletka <podlesskiy2014@gmail.com>
2019-02-19 15:00:52 +0300

    Добавил функцию send_alerts

--- Alerts.py
+++ Alerts.py
@@ -1,4 +1,18 @@
 import users_db_parser
+import Vk_bot
+import Tg_bot
 
-info = users_db_parser.get_user_by_id('vk', 127845402)
-print(info)
+
+def send_alerts(a, mess):
+    vk = []
+    tg = []
+    for id in a:
+        info = users_db_parser.get_user_by_global_id(id)
+        vk_id = info[8]
+        tg_id = info[10]
+        if vk_id:
+            vk.append(vk_id)
+        if tg_id:
+            tg.append(tg_id)
+    Vk_bot.alerts(vk, mess)
+    Tg_bot.alerts(tg, mess)
