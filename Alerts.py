29491029fd01961a3982bc4f097d1440d858fcd9
PetkaKotletka <podlesskiy2014@gmail.com>
2019-02-19 15:09:40 +0300

    Добавил logger

--- Alerts.py
+++ Alerts.py
@@ -1,6 +1,7 @@
 import users_db_parser
 import Vk_bot
 import Tg_bot
+import logger
 
 
 def send_alerts(a, mess):
@@ -14,5 +15,7 @@ def send_alerts(a, mess):
             vk.append(vk_id)
         if tg_id:
             tg.append(tg_id)
+    logger.log("alerts", "alerts formed: vk_users: " + str(len(vk)) + " tg_users: " + str(len(tg)))
+    logger.log("alerts", "alerts formed: vk_ids: " + str(vk) + " tg_ids: " + str(vk))
     Vk_bot.alerts(vk, mess)
     Tg_bot.alerts(tg, mess)
