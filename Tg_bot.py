e17558c426cc569b4af844547dc0a54ee28c328d
Pusenkas <ivanovp09@gmail.com>
2019-02-26 14:34:56 +0300

    Deleted 'URL' from all  functions

--- Tg_bot.py
+++ Tg_bot.py
@@ -9,18 +9,19 @@ TOKEN = config.secret["tg"]["token"]
 URL = 'https://api.telegram.org/bot' + TOKEN + '/'
 raw_msgs = []
 last_msg_id = -1
-super_admins={183534296, 392694288}
+super_admins={183534296}
 
 
 def bot_info():
-    print(URL+' ')
+    print(URL)
     url_get_me = URL + 'getme'
     bot_info = requests.get(url_get_me).json()
     return bot_info
 
 def alerts(alerts_ids, msg):
-    for i in range(len(alerts_ids)):
-        user_id=alerts_ids[i]
+    for i in range(len(allerts_ids)):
+        time.sleep(1)
+        user_id=allerts_ids[i]
         send_msg(user_id, msg)
         logger.log("tg", "sending alert to"+ user_id)
         
@@ -66,7 +67,7 @@ def new_msgs(last_msg_id, raw_msgs):
 
 
 def answer_msg(user_id, msg, msg_id):
-    logger.log("tg", "answering msg - " + msg + " to " +str(user_id))
+    logger.log("tg", "sending message to "+str(user_id))
     result = user_req.parse_message_from_user("tg", user_id, msg)
     msg_to_send = result['text']
     send_msg(user_id, msg_to_send, msg_id)
@@ -83,7 +84,7 @@ def tg_bot_main(last_msg_id, raw_msgs):
                 if msg_to_answer['is_super_admin']=='0':
                     answer_msg(msg_to_answer['tg_user_id'], msg_to_answer['raw_msg'], msg_to_answer['msg_id'])
                 else:
-                    send_msg(msg_to_answer['tg_user_id'], 'you are super admin')
+                    send_msg(msg_to_answer['tg_user_id'], 'you are super admin', msg_to_answer['msg_id'])
                     answer_msg(msg_to_answer['tg_user_id'], msg_to_answer['raw_msg'], msg_to_answer['msg_id'])
             raw_msgs=[] 
 
