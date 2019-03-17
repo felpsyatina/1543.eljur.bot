1e20e417318db0727f6d23fed796fe4bd9b94c36
Pusenkas <ivanovp09@gmail.com>
2019-03-17 19:49:59 +0300

    Bug fixe

--- Tg_bot.py
+++ Tg_bot.py
@@ -80,13 +80,12 @@ def new_msgs(last_msg_id, raw_msgs):
                 super_admin = '0'
             msg = msg_base[i]['message']['text']
             msg_id = msg_base[i]['message']['message_id']
-            logger.log("tg",str(msg_base[i]['message']['from']))
             if 'last_name' in msg_base[i]['message']['from']:
                 user_name = {'first_name': msg_base[i]['message']['from']['first_name'],
                          'last_name': msg_base[i]['message']['from']['last_name']}
             else:
                 user_name = {'first_name': msg_base[i]['message']['from']['first_name'],
-                         'last_name': 'No name'}
+                         'last_name': 'Noname'}
             logger.log("tg", "get message - " + msg + " from" + str(tg_user_id))
             raw_msgs.append({'tg_user_id': tg_user_id, 'raw_msg': msg, 'msg_id': msg_id, 'user_name': user_name,
                              'is_super_admin': super_admin})
