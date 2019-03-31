d1d37bb8096464599a57353f38704ee76814563c
Alex <alexnossofff@gmail.com>
2019-03-31 22:47:01 +0300

    Добавил бэкапилку бд, которая сохраняет базы данных каждый день и удаляет их, если она из позопрошлого месяца.

---
+++ db_copier.py
@@ -0,0 +1,31 @@
+import shutil as sh
+import datetime
+import os
+import config
+import logger
+
+
+def new_db_copy():
+    file_path = config.db_copies_path
+    today = str(datetime.datetime.now())[:10]
+    sh.copy2('1543.eljur.bot.db', file_path + '/database_' + today + '.db')
+
+
+def delete_db():
+    file_path = config.db_copies_path
+    today = str(datetime.datetime.now())[:10]
+    copies_of_db = os.listdir(file_path)
+    for db in copies_of_db:
+        if int(today[5:7]) - int(db[14:16]) == 2 or int(today[5:7]) - int(db[14:16]) == -10:
+            os.remove(file_path + "/" + db)
+
+
+if __name__ == '__main__':
+    while True:
+        try:
+            cur_time = str(datetime.datetime.now())[11:13]
+            if cur_time == "01":
+                new_db_copy()
+                delete_db()
+        except Exception as ex:
+            logger.log("db_copier", f"error {ex}")
