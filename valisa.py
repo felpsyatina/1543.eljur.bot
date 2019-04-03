debc234ab865155af22f2a3d9a8263fae7de924c
Kabakov Ivan <hexwhyzet@gmail.com>
2019-04-04 00:30:38 +0300

    Валиса теперь понимает больше фраз (через n дней/т дней назад). Локально у меня почему-то не подгружются уроки, надо поправлять, сорьки (конфликт либо локальный, либо между valica и user_req).

--- valisa.py
+++ valisa.py
@@ -1,6 +1,8 @@
-from functions import cur_date
 import logger
 import re
+import datetime
+
+
 # Тестовая версия валисы
 
 
@@ -17,9 +19,9 @@ class Valica:
 
         self.type = None
         self.subs = None
-        self.list_of_dates = None
+        self.list_of_dates = []
 
-        self.params_extractors = {"subs": self.extract_class_, "list_of_dates": self.extract_date}
+        self.params_extractors = {"subs": self.extract_class_, "list_of_dates": self.extract_dates}
 
         for cur_definer in self.definers:
             if cur_definer["definer_func"](string):
@@ -35,20 +37,23 @@ class Valica:
 
     def is_get_home_task(self, string):
         string = self.delete_message_from_request(string)
-        keywords = ["домашнее задание", "дз", "домашка"]
 
-        for keyword in keywords:
-            if keyword in string:
+        regexes = [r"домашняя работа", r"дз"]
+
+        for regex in regexes:
+            result = re.search(regex, string)
+            if result is not None:
                 return True
         return False
 
     def is_get_schedule(self, string):
         string = self.delete_message_from_request(string)
 
-        keywords = ["расписание", "расп"]
+        regexes = [r"расписание", r"расп"]
 
-        for keyword in keywords:
-            if keyword in string:
+        for regex in regexes:
+            result = re.search(regex, string)
+            if result is not None:
                 return True
         return False
 
@@ -61,33 +66,52 @@ class Valica:
 
         self.subs = {result.group().strip().upper(): {}}
 
-    def extract_date(self, string):
-        regex = r"(^|\s)(20[1-9][0-9])([,./\\\s]|)([0][1-9]|[1][0-2])([,./\\\s]|)(0[1-9]|[1-2][0-9]|3[0-1])"
-        result = re.search(regex, string)
+    def extract_dates(self, string):
+        # full date is YYYMMDD
+        regex_full_date = r"(((\d{4})(0[13578]|10|12)(0[1-9]|[12][0-9]|3[01]))|((\d{4})(0[469]|11)([0][1-9]|[12][0-9]|30))|((\d{4})(02)(0[1-9]|1[0-9]|2[0-8]))|(([02468][048]00)(02)(29))|(([13579][26]00) (02)(29))|(([0-9][0-9][0][48])(02)(29))|(([0-9][0-9][2468][048])(02)(29))|(([0-9][0-9][13579][26])(02)(29))|(00000000)|(88888888)|(99999999))"
+        result = re.search(regex_full_date, string)
 
-        if result:
-            self.list_of_dates = [result.group().strip()]
+        if result is not None:
+            self.list_of_dates.append(result.group(0))
 
-        add = -2
-        for word in ["позавчера", "вчера", "сегодня", "завтра", "послезавтра"]:
-            if word in string.lower().split():
-                self.list_of_dates = [cur_date(add=add)]
-                return
-            add += 1
+        special_dates = [(r"сегодня", 0),
+                         (r"вчера", -1),
+                         (r"завтра", 1),
+                         (r"позавчера", -2),
+                         (r"послезавтра", 2)]
 
-        return None
+        for regex, delta in special_dates:
+            result = re.search(regex, string)
+            if result is not None:
+                self.list_of_dates.append(self.get_cur_date(delta))
 
-    @staticmethod
-    def delete_message_from_request(string):
-        if string.count('"') > 1:
-            return string[:string.find('"')] + string[string.rfind('"'):]
-        return string
+        regex_custom_delta = [
+            (r"(через|спустя) (-)?[0-9][1-9]* (дня|дней|суток|сутка|сутки)", r"(-)?[0-9][1-9]*", False),
+            (r"(-)?[0-9][1-9]* (дня|дней|суток|сутка|сутки) (до|назад)", r"(-)?[0-9][1-9]*", True)]
+
+        for regex, delta_extract, is_reversed in regex_custom_delta:
+            result = re.search(regex, string)
+            if result is not None:
+                delta = int(re.search(delta_extract, result.group(0)).group(0))
+                if is_reversed:
+                    delta *= -1
+                self.list_of_dates.append(self.get_cur_date(delta))
 
     def get_params(self, string, needed_params):
         params = {}
         for needed_param in needed_params:
             params[needed_param] = self.params_extractors[needed_param](string)
         return params
+
+    @staticmethod
+    def get_cur_date(delta):
+        return (datetime.datetime.today() + datetime.timedelta(days=delta)).strftime('%Y%m%d')
+
+    @staticmethod
+    def delete_message_from_request(string):
+        if string.count('"') > 1:
+            return string[:string.find('"')] + string[string.rfind('"'):]
+        return string
 
 
 if __name__ == '__main__':
