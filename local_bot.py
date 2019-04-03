af083175f210420a9b75196c92d2780e8d37f6da
Kabakov Ivan <hexwhyzet@gmail.com>
2019-04-04 00:20:08 +0300

    Теперь local_bot работает, но весь бот у меня - нет, не подгружается расписагие, возможно просто мои локальные баги

--- local_bot.py
+++ local_bot.py
@@ -4,7 +4,7 @@ if __name__ == '__main__':
     try:
         while True:
             inp = input()
-            ans = user_req.parse_message_from_user("vk", 1, inp, {"first_name": "Пахан", "last_name": "Дуров"})
+            ans = user_req.parse_message_from_user({"src": "vk", "id": 1, "first_name": "Пахан", "last_name": "Дуров", "text": inp})
             print(ans)
     except KeyboardInterrupt:
         pass
