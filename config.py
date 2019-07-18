658c05fab82fa80aa59e672f57f50a2a4cd816d0
IBMOI <hexwhyzet@gmail.com>
2019-07-18 21:23:10 +0300

    Обновил файлик config.py

--- config.py
+++ config.py
@@ -6,18 +6,14 @@ cur_path = os.path.dirname(os.path.abspath(__file__))
 
 secrets_config_file = cur_path + "/configs/config_secrets.json"
 global_config_file = cur_path + "/configs/config.json"
-messages_config_file = cur_path + "/configs/config_messages.json"
 db_copies_path = cur_path + "/db_copies"
 
 
 def load_config(filename):
-	configfile = open(filename, "r", encoding="utf-8")
-	jsonstring = configfile.read()
-	res = json.loads(jsonstring)
-	configfile.close()
-	return res
+	with open(filename, "r", encoding="utf-8") as configfile:
+		res = json.loads(configfile.read())
+		return res
 
 
 params = load_config(global_config_file)
 secret = load_config(cur_path + "/" + params["config_secret_path"])
-mes = load_config(cur_path + "/" + params["config_secret_path"])
